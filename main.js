825a54c3216aa8b23ac3dc72da355746ffd99f6d
uy <a.normurzaev20@gmail.com>
2026-04-06 22:16:11 +0500

    1 masala ishlandi

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-let a =prompt("a soni");
+// let a =+prompt("a soni");
+// let n = +  prompt("n soni");
+
+// function power(a,n) {
+//     let daraja =(a ** n);
+//     console.log(daraja);
+    
+// }
+// power(a,n)
