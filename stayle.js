36eab10ebb621921f0fdc90e99f41f0b03ec045f
uy <a.normurzaev20@gmail.com>
2026-04-07 22:31:03 +0500

    dom yasalmoqda

---
+++ stayle.js
@@ -0,0 +1,11 @@
+
+let container = document.getElementById("container");
+
+let cards = [
+    {title: "Azizbek", text: "Lorem ipsum dolor sit amet."}
+]
+const renderCards = (data)=>{
+    const cards = DataTransfer.map((el)=>`
+    
+`)
+}
