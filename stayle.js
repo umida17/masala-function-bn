9721fdc63ec7e88cd65b4e920846a607219547f5
uy <a.normurzaev20@gmail.com>
2026-04-07 23:00:30 +0500

    data yasalvati

--- stayle.js
+++ stayle.js
@@ -2,13 +2,23 @@
 let container = document.getElementById("container");
 
 let cards = [
-    {title: "Azizbek", text: "Lorem ipsum dolor sit amet."}
-]
-const renderCards = (data)=>{
-    const cards = DataTransfer.map((el)=>`
-     <div key = {el.id} >
-     <h1>${el.title}</h1>
-     <p></p>
-     </div>
-`)
-}
+    { id: 1, title: "Azizbek", text: "Lorem ipsum dolor sit amet." }
+];
+
+const renderCards = (data) => {
+    const result = data.map((el) => `
+        <div class = "card" key ={el.id} >
+            <h1>${el.title}</h1>
+            <img src="https://picsum.photos/id/${el.id}/200/200" alt="">
+            <p>${el.text}</p>
+        </div>
+    `).join("");
+
+    container.innerHTML = result;
+};
+
+renderCards(cards);
+
+
+
+
