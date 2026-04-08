8147802a396b045762e4f0392c6ae34f82e831f6
uy <a.normurzaev20@gmail.com>
2026-04-08 22:17:50 +0500

    11 masala iwlanmoqda

--- main.js
+++ main.js
@@ -158,6 +158,19 @@
 
 
 
+function isPrime(N) {
+    if (N <= 1) return false;
 
+    for (let i = 2; i < N; i++) {
+        if (N % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Misollar:
+console.log("isPrime(7) =>", isPrime(7));   // true
+console.log("isPrime(10) =>", isPrime(10)); // false
 
  
