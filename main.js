3d4d804aa0952aebe1501bf96902881f21e68990
uy <a.normurzaev20@gmail.com>
2026-04-10 23:16:35 +0500

    12 masala ishlanmoqda

--- main.js
+++ main.js
@@ -177,6 +177,31 @@
 
 
 
+function numberOfPrime(N) {
+    let count = 0;
 
+    for (let i = 1; i <= N; i++) {
+        let isPrime = true;
+
+        if (i < 2) {
+            isPrime = false;
+        } else {
+            for (let j = 2; j < i; j++) {
+                if (i % j === 0) {
+                    isPrime = false;
+                }
+            }
+        }
+
+        if (isPrime) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+// Misol:
+console.log(numberOfPrime(10));
 
  
