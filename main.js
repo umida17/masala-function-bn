9220ad6c23e6940909146e11e5c8560b4f3d38e7
uy <a.normurzaev20@gmail.com>
2026-04-14 23:49:10 +0500

    20 masala ishlanmoqda

--- main.js
+++ main.js
@@ -329,3 +329,12 @@ function decTime(H, M, S) {
     if (H < 0) {
         H = 23;
     }
+
+
+    // format (2 xonali qilish)
+    let hh = String(H).padStart(2, '0');
+    let mm = String(M).padStart(2, '0');
+    let ss = String(S).padStart(2, '0');
+
+    return `${hh}:${mm}:${ss}`;
+}
